ec43209002d6753f2229f81b26270169d0fda1be
Shyam Menon <shyam.menon@hp.com>
2024-09-25 14:55:40 +0530

    Aspire sample changes to use Azure Open AI along with Open AI

--- AspireSample.Web/Program.cs
+++ AspireSample.Web/Program.cs
@@ -11,17 +11,50 @@ builder.AddServiceDefaults();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
-if (string.IsNullOrEmpty(apiKey))
+var useOpenAPI = true;
+string? apiKey = string.Empty;
+
+if (useOpenAPI)
 {
-    throw new InvalidOperationException("The OpenAI API key is not set in the environment variables.");
+    // Get the Open AI API key from environment variables
+    apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+    if (string.IsNullOrEmpty(apiKey))
+    {
+        throw new InvalidOperationException("The OpenAI API key is not set in the environment variables.");
+    }
+
+    //Add OpenAI chat completion service
+    builder.Services.AddKernel().AddOpenAIChatCompletion(
+        modelId: "gpt-4o-mini",
+        apiKey: apiKey
+    );
+}
+else
+{
+    // Get the Azure API key from environment variables
+    apiKey = Environment.GetEnvironmentVariable("AZURE_API_KEY");
+
+    if (string.IsNullOrEmpty(apiKey))
+    {
+        Console.WriteLine("Please set the AZURE_API_KEY environment variable.");
+        return;
+    }
+
+    var endpoint = Environment.GetEnvironmentVariable("AZURE_ENDPOINT");
+    if (string.IsNullOrEmpty(endpoint))
+    {
+        Console.WriteLine("Please set the AZURE_ENDPOINT environment variable.");
+        return;
+    }
+
+    //Add Azure Open AI chat completion service
+    builder.Services.AddKernel().AddAzureOpenAIChatCompletion("GPT-4o",
+    endpoint,
+    apiKey,
+    "GPT-4o");
+
 }
 
-//Add OpenAI chat completion service
-builder.Services.AddKernel().AddOpenAIChatCompletion(
-    modelId: "gpt-4o-mini",
-    apiKey: apiKey
-);
 
 //Create a theme plugin from the ThemePlugin class
 builder.Services.AddScoped(sp => KernelPluginFactory.CreateFromType<ThemePlugin>(serviceProvider: sp));
